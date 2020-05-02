97993c0931ef8dd8f47f1d0f854826aa4bf565f6
Ali Asger Modi <modialiasger@gmail.com>
2020-05-03 01:52:38 +0300

    added screens

---
+++ src/scenes/home/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {RootSafeAreaView, RootView, RootScrollView} from '_styles/RootView';
+import {
+  Title,
+  Container,
+  CategoryContainer,
+  CategoryCard,
+  ImageView,
+} from '_styles/homeScreen';
+import Carousel from '_components/carousel';
+import {useNavigation} from '@react-navigation/native';
+
+const home = () => {
+  return (
+    <RootSafeAreaView>
+      <RootScrollView>
+        <RootView>
+          <Container>
+            <Carousel />
+          </Container>
+          <Container>
+            <Title>Category</Title>
+            <Category />
+          </Container>
+        </RootView>
+      </RootScrollView>
+    </RootSafeAreaView>
+  );
+};
+
+const Category = () => {
+  const navigation = useNavigation();
+
+  return (
+    <CategoryContainer>
+      <CategoryCard
+        onPress={() => {
+          navigation.navigate('ProductList');
+        }}>
+        <ImageView
+          source={require('_assets/images/shoes.jpg')}
+          resizeMode="contain"
+        />
+      </CategoryCard>
+      <CategoryCard
+        onPress={() => {
+          navigation.navigate('ProductList');
+        }}>
+        <ImageView
+          source={require('_assets/images/jeans.jpg')}
+          resizeMode="stretch"
+        />
+      </CategoryCard>
+    </CategoryContainer>
+  );
+};
+
+export default home;
---
+++ src/scenes/productDetails/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {RootSafeAreaView, RootView, RootScrollView} from '_styles/RootView';
+
+const ProductDetails = () => {
+  return (
+    <RootSafeAreaView>
+      <RootScrollView></RootScrollView>
+    </RootSafeAreaView>
+  );
+};
+
+export default ProductDetails;
