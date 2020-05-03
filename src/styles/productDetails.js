86a97ab420e4ad68e13e8c3e537c6aaa210063b3
Ali Asger Modi <modialiasger@gmail.com>
2020-05-03 14:42:30 +0300

    static data removed

--- src/styles/productDetails.js
+++ src/styles/productDetails.js
@@ -1,17 +1,15 @@
 import styled from 'styled-components/native';
 import {scaleSize, scaleFont} from './mixins';
-import {FONT_SIZE_16, FONT_SIZE_12} from './typography';
 
 export const Card = styled.TouchableOpacity`
   padding: ${scaleSize(20)}px;
   border: 1px solid #ede8df;
   border-radius: ${scaleSize(3)}px;
   margin-bottom: 10px;
-  /* background-color: red; */
 `;
 
 export const ImageView = styled.Image`
-  height: ${scaleSize(200)}px;
+  height: ${scaleSize(250)}px;
   width: 100%;
 `;
 
@@ -32,7 +30,7 @@ export const Description = styled.Text`
 `;
 
 export const Price = styled.Text`
-  font-size: ${scaleFont(18)}px;
+  font-size: ${scaleFont(20)}px;
   color: #424242;
   text-align: justify;
   font-weight: 700;
