c1b7848cfd1b006be3dad2f1c397b3ad3225dad9
Ali Asger Modi <modialiasger@gmail.com>
2020-05-03 15:59:45 +0300

    adding location comps

---
+++ src/components/location/index.js
@@ -0,0 +1,26 @@
+import React, {useEffect, useState} from 'react';
+import {View, Text} from 'react-native';
+import {Container, LocationText} from '_styles/location';
+//import Icon from 'react-native-vector-icons/Entypo';
+import {scaleFont} from '_styles/mixins';
+import GetCurrentLocation from '_utils/GeoLocation';
+
+//const locationIcon = (
+  //<Icon name="location-pin" size={scaleFont(35)} color="#900" />
+//);
+
+const CurrentLocation = () => {
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    const currentLocation = GetCurrentLocation();
+  }, []);
+
+  return (
+    <Container>
+      <LocationText>Bengaluru</LocationText>
+    </Container>
+  );
+};
+
+export default CurrentLocation;
