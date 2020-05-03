4ed6b9bfe8fa7b8a9500eb6b2c98c4edf63b2ffb
Ali Asger Modi <modialiasger@gmail.com>
2020-05-03 12:56:05 +0300

    path updation

--- src/scenes/productDetails/index.js
+++ src/scenes/productDetails/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-import {RootSafeAreaView, RootView, RootScrollView} from '_styles/RootView';
+import {RootSafeAreaView, RootView, RootScrollView} from '_styles/RootView.js';
 
 const ProductDetails = () => {
   return (
