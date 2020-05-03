1cc5d8551c1db359fa4c39268ace33316ab1468d
Ali Asger Modi <modialiasger@gmail.com>
2020-05-03 14:44:24 +0300

    json data added

---
+++ src/scenes/productList/data.js
@@ -0,0 +1,45 @@
+export const shoesData = [
+    {
+      id: '1',
+      title: 'Adidas Stan Smith-leather classic',
+      desc:
+        'A style icon is remade for younger feet in these adidas Originals Superstar shoes. This pair has all the classic details, including the smooth leather ...',
+      price: '$60',
+      image: require('../../assets/images/shoes/1.jpg'),
+    },
+    {
+      id: '2',
+      title: 'Adidas SuperStar Shoes',
+      desc:
+        'Originally made for basketball courts in the 70s. Celebrated by hip hop royalty in the 80s. The adidas Superstar shoe is now a lifestyle staple for ...',
+      price: '$80',
+      image: require('../../assets/images/shoes/2.jpg'),
+    },
+    {
+      id: '3',
+      title: 'Adidas Campus 2',
+      desc: 'adidas Campus 80s "Japan Pack Vintage ...',
+      price: '$90',
+      image: require('../../assets/images/shoes/3.jpg'),
+    },
+  ];
+  
+  export const denimData = [
+    {
+      id: '1',
+      title: 'Levis 501 Original Fit Selvedged Jeans',
+      desc:
+        'The 501® is an iconic straight fit with the signature button fly since 1873. This one features premium fabric and finishing for the Levis® Made & ...',
+      price: '$100',
+      image: require('../../assets/images/denim/denim1.jpg'),
+    },
+  
+    {
+      id: '2',
+      title: 'Levis Made in Japan 511™ Slim Fit Jeans',
+      desc:
+        'These Made in Japan™ premium crafted denim jeans have been hand dyed and finished by some of Japan’s most admired artists. A modern slim with room to ...',
+      price: '$105',
+      image: require('../../assets/images/denim/denim2.jpg'),
+    },
+  ];
--- src/scenes/productList/index.js
+++ src/scenes/productList/index.js
@@ -1,41 +1,31 @@
 import React from 'react';
 import {FlatList} from 'react-native';
-import {RootSafeAreaView, RootView, RootScrollView} from '_styles/RootView.js';
+import {RootSafeAreaView, RootView, RootScrollView} from '_styles/RootView';
 import ProductCard from '_components/product-card';
+import {shoesData, denimData} from './data';
 
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-];
-
-const ProductList = () => {
+const ProductList = ({route}) => {
+  const {type} = route.params;
   return (
     <RootSafeAreaView>
       <RootView>
-        <List />
+        <List type={type} />
       </RootView>
     </RootSafeAreaView>
   );
 };
 
-function List() {
+const List = (props) => {
+  const {type} = props;
+
   return (
     <FlatList
-      data={DATA}
-      renderItem={({data}) => <ProductCard data={data} />}
+      data={type === 'shoes' ? shoesData : denimData}
+      renderItem={(data) => <ProductCard data={data} />}
       keyExtractor={(data) => data.id}
+      showsVerticalScrollIndicator={false}
     />
   );
-}
+};
 
 export default ProductList;
